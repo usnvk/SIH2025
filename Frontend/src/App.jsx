e3ea2a0d7a9e197b7751d48121ce4f88960da8cd
usnvk <usnvk8888@gmail.com>
2025-09-27 09:35:44 +0530

    1st commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+import DietPlanGenerator from "./components/DietPlanGenerator";
+
+function App() {
+  return (
+    <div>
+      <DietPlanGenerator />
+    </div>
+  );
+}
+
+export default App;
